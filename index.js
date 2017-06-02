59aba79cbb31cd380ee661ae8d82d6ba1356f337
Douglas Matoso <doug2k1@gmail.com>
2017-06-01 21:00:09 -0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,14 @@
+function hello (name) {
+  var message = 'Olá';
+
+  console.log(mensagem + ", " + name + "!");
+}
+
+function timeNow () {
+  var now = new Date()
+  var xpto = 2
+
+  console.log(now.getHours() + ':' + now.getMinutes())
+}
+
+timeNow()
